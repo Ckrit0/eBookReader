192cb3077909d1d3be47565ff5f634034f3e00f8
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-11-28 00:15:08 +0900

    Wrap data in HTML and enable SSL for Flask app
    Modify the main function to wrap data in HTML paragraph tags and enable SSL for the Flask app.

--- host.py
+++ host.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def main():
   data=db.getData()
+  result = "<p>" + data + "</p>"
   return  data
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", ssl_context='adhoc')
+
