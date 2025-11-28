76976a71e762c21929f7608b4cc7a66f884b68bf
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-11-29 03:47:02 +0900

    Update Flask app to use templates and add routes
    Refactor main function to render HTML templates and add selectVolume route.

--- host.py
+++ host.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlCRUD as db
 import requests
 
-def get_external_ip():
+def get_external_ip(): # 나중에 지울 예정
   try:
     response = requests.get('https://api.ipify.org')
     external_ip = response.text
@@ -10,32 +10,30 @@ def get_external_ip():
   except Exception as e:
     print('Error: ',e)
     return 'ip_check_error'
+  
+# 변수 모음
+bookInfo = {
+  'name' : '',
+  'value' : 0,
+  'line' : 0
+}
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
   data=db.getData()
-  result = ""
-  for row in data:
-    for value in row:
-      result = result + str(value)
-  return  result
+  return  render_template('index.html',data=data)
+
+@app.route("/<bookName>")
+def selectVolume(bookName):
+  bookInfo['name'] = bookName
+  volumes = [1,2,3,4,5] # 이거 나중엔 서버에서 받아와야 함
+  return render_template('selectVolume.html', bookInfo=bookInfo, volumes=volumes)
 
 if __name__ == '__main__':
-  print('############외부ip:',get_external_ip() + '############')
+  print('############외부ip:',get_external_ip() + '############') # 나중에 지울 예정
   app.run(
     debug=True
     , host='0.0.0.0'
   )
-
-
-
-
-
-
-
-
-
-
-
