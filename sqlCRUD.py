663b8fe82e362d46288a7251f60d238bbb53a996
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-12-16 00:45:17 +0900

    Update sqlCRUD.py

--- sqlCRUD.py
+++ sqlCRUD.py
@@ -46,9 +46,9 @@ def getData(sql):
 def getBookList():
   sql = f"SELECT * FROM INFO"
   bookList = getData(sql=sql)
+  print('bookList:',bookList)
   for book in bookList:
      book = book[1]
-  print('bookList:',bookList)
   return bookList
 
 # 해당 도서 마지막권(화) 받아오기
@@ -56,22 +56,21 @@ def getLastVolume(bookName):
   sql = f"SELECT MAX(VOLUME) FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME='{bookName}')"
   lastVolume = getData(sql)
   lastVolume = lastVolume[0][0]
-  print('lastVolume:',lastVolume)
   return lastVolume
 
 # 해당 도서 모든 권(화) 받아오기
 def getVolumes(bookName):
   sql = f"SELECT DISTINCT VOLUME FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME='{bookName}')"
-  volumeList = list(getData(sql=sql))
+  volumeList = getData(sql=sql)
   for volume in volumeList:
-     volume = volume[0][0]
+     volume = volume[0]
   print('volumeList:',volumeList)
   return volumeList
 
 # 해당 도서 해당 권(화) 내용 받아오기
 def getContents(bookName, volume):
   sql = f"SELECT CONTENTS FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME='{bookName}') AND VOLUME={volume} ORDER BY LINE ASC"
-  lineList = list(getData(sql=sql))
+  lineList = getData(sql=sql)
   for line in lineList:
      line = line[0]
   print('lineList:',lineList)
@@ -82,5 +81,4 @@ def getAdminPw():
   sql = f"SELECT PASSWORD FROM OPTIONS"
   pw = getData(sql=sql)
   pw = pw[0][0]
-  print('pw:',pw)
   return pw
