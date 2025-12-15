a6e5bee9e78165a6a2c171bb67f219ca6b6d189e
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-12-16 01:48:20 +0900

    insertBook 추가

--- sqlCRUD.py
+++ sqlCRUD.py
@@ -1,42 +1,52 @@
 import pymysql
 
 dbInfo = {
-    'host' : 'localhost',
-    'port' : 3306,
-    'user' : 'root',
-    'password' : 'Whffkaos1!',
-    'database' : 'E_BOOK_READER'
+  'host' : 'localhost',
+  'port' : 3306,
+  'user' : 'root',
+  'password' : 'Whffkaos1!',
+  'database' : 'E_BOOK_READER'
 }
 
 def getCursor():
-    global dbInfo
-    con = None
-    cur = None
-    try:
-        con = pymysql.connect(
-            host=dbInfo['host'],
-            port=dbInfo['port'],
-            user=dbInfo['user'],
-            password=dbInfo['password'],
-            database=dbInfo['database']
-        )
-        cur = con.cursor()
-    except Exception as e:
-        print('Connect Error:',e)
-    return con, cur
+  global dbInfo
+  con = None
+  cur = None
+  try:
+    con = pymysql.connect(
+      host=dbInfo['host'],
+      port=dbInfo['port'],
+      user=dbInfo['user'],
+      password=dbInfo['password'],
+      database=dbInfo['database']
+    )
+    cur = con.cursor()
+  except Exception as e:
+      print('Connect Error:',e)
+  return con, cur
 
 def getData(sql):
-    data = []
-    con, cur = getCursor()
-    if con == None:
-        return None
-    cur.execute(sql)
-    result = cur.fetchall()
-    for row in result:
-        for r in row:
-          data.append(r)
-    con.close()
-    return data
+  data = []
+  con, cur = getCursor()
+  if con == None:
+    return None
+  cur.execute(sql)
+  result = cur.fetchall()
+  for row in result:
+    for r in row:
+      data.append(r)
+  con.close()
+  return data
+
+def setData(sql):
+  con, cur = getCursor()
+  if con == None:
+    return None
+  cur.execute(sql)
+  result = cur.fetchall()
+  con.commit()
+  con.close()
+  return result
 
 
 ################
@@ -57,7 +67,7 @@ def getLastVolume(bookName):
 
 # 해당 도서 모든 권(화) 받아오기
 def getVolumes(bookName):
-  sql = f"SELECT DISTINCT VOLUME FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME='{bookName}')"
+  sql = f"SELECT DISTINCT VOLUME FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME='{bookName}') ORDER BY VOLUME"
   volumeList = getData(sql=sql)
   return volumeList
 
@@ -72,3 +82,9 @@ def getAdminPw():
   sql = f"SELECT PASSWORD FROM OPTIONS"
   pw = getData(sql=sql)
   return pw[0]
+
+def insertBook(bookName):
+  sql = f"INSERT INTO INFO VALUES(IFNULL((SELECT MAX(BID)+1 FROM INFO),0),'{bookName}')"
+  result = setData(sql=sql)
+  print(result)
+  return result
