91980434956d90df034d52a65ec1e9cf1d634f97
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-12-17 05:02:49 +0900

    db 연동 기능 구현(c SQL)

--- sqlCRUD.py
+++ sqlCRUD.py
@@ -3,8 +3,8 @@ import pymysql
 dbInfo = {
   'host' : 'localhost',
   'port' : 3306,
-  'user' : 'root',
-  'password' : 'Whffkaos1!',
+  'user' : 'ckrit',
+  'password' : 'P@ssw0rd',
   'database' : 'E_BOOK_READER'
 }
 
@@ -58,6 +58,12 @@ def setData(sql):
 ## DB 연결부분 ##
 ################
 
+# 관리자 비번 받아오기
+def getAdminPw():
+  sql = f"SELECT PASSWORD FROM OPTIONS"
+  pw = getData(sql=sql)[0]
+  return pw
+
 # 도서 목록 받아오기
 def getBookList():
   sql = f"SELECT NAME FROM INFO ORDER BY NAME ASC"
@@ -82,13 +88,58 @@ def getContents(bookName, volume):
   lineList = getData(sql=sql)
   return lineList
 
-# 관리자 비번 받아오기
-def getAdminPw():
-  sql = f"SELECT PASSWORD FROM OPTIONS"
-  pw = getData(sql=sql)[0]
-  return pw
-
+# 도서 추가하기
 def insertBook(bookName):
   sql = f"INSERT INTO INFO VALUES((SELECT IFNULL((SELECT MAX(BID)+1 FROM INFO),0)),'{bookName}')"
   setData(sql=sql)
-  return
+
+# 도서명 변경하기
+def updateBook(bookName,newName):
+  sql = f"UPDATE INFO SET NAME={newName} WHERE NAME={bookName}"
+  setData(sql=sql)
+
+# 도서 삭제하기
+def deleteBook(bookName):
+  # 내용 삭제
+  sql = f"DELETE FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME={bookName})"
+  setData(sql=sql)
+  # 도서정보 삭제
+  sql = f"DELETE FROM INFO WHERE NAME={bookName}"
+  setData(sql=sql)
+
+# 볼륨 컨텐츠 추가하기(수정하기)
+def insertVolume(bookName,volume,contentList):
+  # 기존 내용 삭제
+  sql = f"DELETE FROM CONTENT WHERE BID=(SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume}"
+  setData(sql=sql)
+  # 새로운 내용 추가
+  sql = f"INSERT INTO CONTENT VALUES("
+  for i in range(len(contentList)):
+    sql = sql + f"((SELECT BID FROM INFO WHERE NAME={bookName}),{volume},{i+1},{contentList[i]})"
+    if i < len(contentList)-1:
+      sql = sql + f", "
+  sql = sql + ")"
+  setData(sql=sql)
+
+# 볼륨 수정하기
+def updateVolume(bookName,volume,newVolume):
+  sql = f"UPDATE CONTENT SET VOLUME = {newVolume} WHERE BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume}"
+  setData(sql=sql)
+
+# 볼륨 삭제하기
+def deleteVolume(bookName,volume):
+  sql = f"DELETE CONTENT WHERE BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume}"
+  setData(sql=sql)
+
+# 라인 수정하기
+def updateContent(bookName,volume,line,content):
+  sql = f"UPDATE CONTENT SET CONTENT = {content} WHERE BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume} AND LINE = {line}"
+  setData(sql=sql)
+
+# 라인 삭제하기
+def deleteContent(bookName,volume,line):
+  sql = f"DELETE CONTENT WHERE  BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume} AND LINE = {line}"
+  setData(sql=sql)
+  # 이후 라인 땡기기
+  sql = f"UPDATE CONTENT SET LINE = ((SELECT LINE FROM CONTENT WHERE BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume} AND LINE = {line}) - 1) WHERE BID = (SELECT BID FROM INFO WHERE NAME={bookName}) AND VOLUME = {volume} AND LINE > {line}"
+  setData()
