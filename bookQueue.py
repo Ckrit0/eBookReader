4caa7ca388130ddc0eb1b75dcbe1fb055553811b
Ckrit0 <104796035+Ckrit0@users.noreply.github.com>
2025-12-20 03:32:43 +0900

    왜?? 왜 이렇게 나오지..?

--- bookQueue.py
+++ bookQueue.py
@@ -62,12 +62,12 @@ class volumeForInsert:
         return math.floor((len(chunkList) * 100) / int(totalChunk))
     
     def setContents(self, contentDict):
+        print(contentDict)
         for key in contentDict.keys():
             if key == "chunkIndex":
                 self.contents["chunkIndex"].append(contentDict[key])
             else :
                 self.contents[key] = contentDict[key]
-                print(contentDict[key])
         return self.checkSetContents()
     
     def getContents(self):
